43cc74c3f8ca1a82699698c9ee0fda5378b071bc
Ruslan Gilmutdinov <ruslangil@mail.ru>
2017-11-22 00:26:31 +0600

    exercise 2

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+		readonly List<Customer> _customers = new List<Customer>
+		{
+			new Customer { Id = 1, Name = "John Smith" },
+			new Customer { Id = 2, Name = "Marry Williams" }
+		};
+
+		[Route("customers")]
+		public ViewResult Index()
+		{
+			return View(this._customers);
+		}
+
+		[Route("customers/{id}")]
+		public ActionResult Details(int id)
+		{
+			Customer customer = this._customers.FirstOrDefault(c => c.Id == id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(customer);
+		}
+	}
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,60 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ViewResult Random()
+        [Route("movies")]
+        public ViewResult Index()
         {
-            Movie movie = new Movie
+            List<Movie> movies = new List<Movie>
             {
-                Name = "Shrek!"
+                new Movie {Name = "Shrek"},
+                new Movie {Name = "Wall-e"}
             };
 
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
-
-        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        //public ActionResult ByReleaseDate(int year, int month)
-        //{
-        //    return Content(year + "/" + month);
-        //}
-
-        //public ActionResult Edit(int id)
-        //{
-        //    return Content("id=" + id);
-        //}
-
-        //// movies
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //    {
-        //        pageIndex = 1;
-        //    }
-
-        //    if (string.IsNullOrWhiteSpace(sortBy))
-        //    {
-        //        sortBy = "Name";
-        //    }
-
-        //    return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
-        //}
     }
 }
